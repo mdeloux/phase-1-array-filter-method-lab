3ae38a246171f5136b358bc52ababa3c77cb30d9
matt <mdeloux@gmail.com>
2022-01-16 13:17:19 -0600

    commit tree a4007ba75c88374e3251a98ac0e22d05497e7331

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(collection) {
+    for (const user of collection) {
+        if (firstName(user)) {
+            return (user.name);
+        }
+    }
+}
+function firstName(user) {
+    return user['drivers'];
+}
+
+Filter(drivers);
